a9791783a2713aab288d6fc70b20def5f1b595b3
alrang0929 <159221214+alrang0929@users.noreply.github.com>
2024-12-03 18:18:13 +0900

    ㅁㄴㅇ

--- project-app/backend/log/errorMessages.js
+++ project-app/backend/log/errorMessages.js
@@ -1,7 +1,7 @@
 const errorMessages = {
-    loginFailed: '아이디 또는 비밀번호가 잘못되었습니다.',
-    userNotFound: '사용자를 찾을 수 없습니다.',
-    serverError: '서버 오류가 발생했습니다. 나중에 다시 시도해주세요.',
+    loginFailed: '아이디 또는 비밀번호를 확인해주세요',
+    userNotFound: '사용자를 찾을 수 없습니다',
+    serverError: '서버 오류가 발생했습니다. 잠시후 다시 시도해주세요',
   };
   
   export default errorMessages;
